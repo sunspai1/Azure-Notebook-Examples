3e793be8e2b89da2b40fca350c974dd33e4c7565
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-09-22 21:35:36 +0000

    save

--- cli/jobs/pipelines/basics/src/test.py
+++ cli/jobs/pipelines/basics/src/test.py
@@ -2,6 +2,8 @@ import os
 import mlflow
 
 os.system('echo "hello world" > helloworld.txt')
+print(os.environ)
+print(os.environment)
 
 mlflow.log_param("hello", "world")
 mlflow.log_metric("hello_factor", 0.9)
